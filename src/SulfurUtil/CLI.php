50485de6894caf4888f5a0d8c3cfcd680356a7e6
Cameron Jacobson <cameron@datashovel.com>
2012-12-27 23:30:36 -0500

    repl command parser and moved sulfur app into 'bin' directory

--- src/SulfurUtil/CLI.php
+++ src/SulfurUtil/CLI.php
@@ -41,11 +41,18 @@ class CLI extends Command
 				'i',
 				InputOption::VALUE_REQUIRED,
 				'Which elasticsearch index?'
+			)
+			->addOption(
+				'debug',
+				'd',
+				InputOption::VALUE_NONE,
+				'Debug Mode?'
 			);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
+		define('DEBUG_MODE',$input->getOption('debug'));
 		$context = new SulfurContext();
 		$context->host = $input->getOption('host');
 		$context->port = $input->getOption('port');
@@ -55,7 +62,10 @@ class CLI extends Command
 		do{
 			$output->write('<fg=yellow>sulfur > </fg=yellow>');
 			$cmd = fgets(STDIN,1024);
-			switch(trim($cmd)){
+			$arguments = $this->parseCommand($cmd);
+			$command = array_shift($arguments);
+			$command = trim($command);
+			switch($command){
 				case 'exit':
 					die('you exited'.PHP_EOL);
 					break;
@@ -63,10 +73,76 @@ class CLI extends Command
 					$a = `echo "# Enter your command:\n" | vipe | tee`;
 					$output->write($a);
 					break;
+				case 'new':// context,query
+					$context = new SulfurContext();
+					$output->writeln('You said: '.$command);
+					break;
+				case 'delete':// query
+					$output->writeln('You said: '.$command);
+					break;
+				case 'get': // query
+					$output->writeln('You said: '.$command);
+					break;
+				case 'set': // query
+					$output->writeln('You said: '.$command);
+					break;
+				case 'show': // url,{apicall}
+					$output->writeln('You said: '.$command);
+					break;
+				case 'help': // {apicall}
+					$output->writeln('You said: '.$command);
+					break;
+				case 'load': // template
+					$output->writeln('You said: '.$command);
+					break;
+				case 'show':// template
+					$output->writeln('You said: '.$command);
+					break;
+				case 'parse': // template
+					$output->writeln('You said: '.$command);
+					break;
+				case '{apicall}':
+					$output->writeln('You said: '.$command);
+					break;
+				case 'json':
+					$output->writeln('You said: '.$command);
+					break;
 				default:
-					$output->write('You said: '.$cmd);
+					$output->writeln('You said: '.$command);
 					break;
 			}
+if(DEBUG_MODE){
+			$output->writeln('ARGUMENTS:');
+			foreach($arguments as $v){
+				$output->writeln("\t".$v);
+			}
+}
 		} while(true);
+	}
+	protected function parseCommand($cmd){
+		$components = [];
+		$tokenized = token_get_all('<?php '.$cmd);
+		foreach($tokenized as $token){
+			switch(token_name($token[0])){
+				case 'T_OPEN_TAG':
+				case 'T_WHITESPACE':
+					break;
+				default:
+					$tmp = trim($token[1]," \t\n\r\0\x0B");
+
+					if(strpos($tmp,'"') === 0){
+						$tmp = trim($tmp,'"');
+					}
+					elseif(strpos($tmp,"'") === 0){
+						$tmp = trim($tmp,"'");
+					}
+
+					if(!empty($tmp)){
+						$components[] = $tmp;//trim($token[1]," \t\n\r\0\x0B\"'");
+					}
+					break;
+			}
+		}
+		return $components;
 	}
 }
